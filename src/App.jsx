1233c1310ce3b37a84a3aa7027a088bc18130461
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2025-05-05 15:40:14 +0530

    Removed old files and started using React

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import style from "./style.module.css"
+function App(){
+  
+  return(
+    <>
+      <div className={style.head}>
+
+      </div>
+      <div className={style.skills}>
+
+      </div>
+      <div className={style.projects}>
+
+      </div>
+      <div className={style.contact}>
+        
+      </div>
+    </>
+  )
+}
+
+export default App;
